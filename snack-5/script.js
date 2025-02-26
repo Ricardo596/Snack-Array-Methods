baaae9446f592f44d1f46147be684c95905688a2
Ricardo596 <ricardodiotti8@gmail.com>
2025-02-26 23:10:52 +0100

    Non è perfetto pero vengono pari):

--- snack-5/script.js
+++ snack-5/script.js
@@ -1,4 +1,13 @@
 const nums = [2, 8, 4, 7, 12, 87];
 
+const even = [];
+for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 2===0)
+        even.push(nums[i])
+    console.log(even)
+}
+
+
+
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
